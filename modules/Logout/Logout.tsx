51086f66cf1abf7a866db4b8704cca76c6aa940d
Kenny Krosky <kenny.krosky@gmail.com>
2024-06-15 08:34:46 -0700

    bump

--- modules/Logout/Logout.tsx
+++ modules/Logout/Logout.tsx
@@ -1,12 +1,10 @@
 "use client";
-
 import { signOut } from "next-auth/react";
 
 export const Logout = () => {
   return (
     <button
       onClick={() => {
-        console.log("hi");
         signOut({ callbackUrl: process.env.NEXT_PUBLIC_URL });
       }}
     >
